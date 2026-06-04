2ec54d53f7da339747eb5c46874815911d65092e
DhamodharThyiagarajan <dhamodcruz1@gmail.com>
2026-06-05 03:25:47 +0530

    Update Navbar.jsx

--- src/Components/layout/Navbar.jsx
+++ src/Components/layout/Navbar.jsx
@@ -103,7 +103,7 @@ const Navbar = () => {
         </div>
       )}
 
-      <h1 className="text-yellow-400 font-bold text-lg md:text-xl lg:text-2xl text-center w-full"></h1>
+      
     </nav>
   );
 };
